f26b6ea1c80bf863e3de77270b17a77b633a6972
Mojtaba Sarooghi <mosa@queue-it.com>
2020-03-02 15:28:59 +0100

    release v3.6

--- ViewerRequest/queueitHelpers.js
+++ ViewerRequest/queueitHelpers.js
@@ -1,4 +1,4 @@
-const CLOUDFRONT_SDK_VERSION ="1.1.1";
+const CLOUDFRONT_SDK_VERSION ="1.2.0";
 const QueueIT = require("./sdk/queueit-knownuserv3-sdk.js");
 exports.addKUPlatformVersion= function(redirectQueueUrl)
 {
